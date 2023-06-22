1d4431bc851a44a755b9854ea2f1ac5682a8338f
marinabell1 <marinabell@estudante.ufscar.br>
2023-06-22 10:03:36 -0300

    die rolling
    using function sample to roll die

---
+++ R commands/Hands-on-programming.R
@@ -0,0 +1,49 @@
+###############################################################
+#
+#That’s the basic interface for executing R code in RStudio. 
+#   Think you have it? If so, try  doing these simple tasks. 
+#If you execute everything correctly, you should end up with
+#the same number that you started with:
+#  1. Choose any number and add 2 to it.
+#  2. Multiply the result by 3.
+#  3. Subtract 6 from the answer.
+#  4. Divide what you get by 3.
+
+################################################################
+
+7 + 2
+#[1]*3
+9*3
+27 - 6
+21/3
+
+#succeded :)
+
+################################################################
+
+
+die <- 1:6
+die - 1
+
+#functions are mechanism for processing thing in an optimal way
+#there are functions already built, but we can also built our own
+#the data out in the function is it's argument
+#function sample helps us roll our die
+
+sample(x = die, size=1 )
+sample(x = die, size=1 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
